42bf247e3a5d85c786d89276ea56bad48e0fc52a
Steve Rogers <32861078+steverogers180@users.noreply.github.com>
2024-04-14 11:32:11 +0000

    fix: gracefully closing the server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import http from "http";
+import type { Logger } from "winston";
 import { App } from "./app";
 import { appDependencies } from "./common/dependencyInjections";
+
+interface CloseServerParams {
+  logger: Logger;
+  server: http.Server;
+}
+
+async function closeServer(params: CloseServerParams) {
+  const { logger, server } = params;
+  server.close(() => {
+    logger.info("HTTP server closed.");
+    process.exit(0);
+  });
+
+  // Forcefully close connections after a timeout
+  setTimeout(() => {
+    logger.error("Forcing server shutdown");
+    process.exit(1);
+  }, 5000);
+}
 
 async function main() {
   const { appRouter, globalErrorHandlerMiddleware, host, logger, port } =
@@ -18,6 +38,14 @@ async function main() {
 
   httpServer.on("listening", async () => {
     logger.info(`server listening to ${host}:${port}`);
+  });
+
+  process.on("SIGINT", async () => {
+    closeServer({ logger, server: httpServer });
+  });
+
+  process.on("SIGTERM", async () => {
+    closeServer({ logger, server: httpServer });
   });
 
   process.on("unhandledRejection", (reason: Error) => {
